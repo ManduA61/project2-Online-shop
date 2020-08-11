19595303a82c08e7598763754117769b5e9e6cad
ManduA61 <ManduA61@gmail.com>
2020-08-12 00:42:58 +0300

    Add all pages in jsx and working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, Switch} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Login from './pages/Login';
+import Page404 from './pages/Page404';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path='/' component={Home} exact/>
+        <Route path='/about' component={About}/>
+        <Route path='/login' component={Login}/>
+
+        <Route path='*' component={Page404}/>
+     </Switch>
     </div>
   );
 }
